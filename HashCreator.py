e311e30daf978af58ce00a63d60d2bdd1eca6d1c
Vectis-45 <76608635+Vectis-45@users.noreply.github.com>
2021-05-25 01:09:03 -0600

    Update HashCreator.py

--- HashCreator.py
+++ HashCreator.py
@@ -2,10 +2,12 @@ import hashlib
 
 
 str2hash = input('Enter string to hash: ')
+str2hash_length = len(str2hash)
 
 result = hashlib.md5(str2hash.encode())
 
 
-print("The hexadecimal equivalent of hash is : ", end="")
-print(result.hexdigest())
-
+print("The hexadecimal equivalent of hash is : " + result.hexdigest())
+print("Hash length: " + str(str2hash_length))
+if str2hash_length > 6:
+    print("WARNING: Your hash is over 6 characters long and may take a while to solve")
