4124cab566555dc3fcc6e17fe9f10b701d58d9d9
Vectis-45 <76608635+Vectis-45@users.noreply.github.com>
2021-05-24 21:36:14 -0700

    Update HashSolver.py

--- HashSolver.py
+++ HashSolver.py
@@ -2,18 +2,27 @@ import hashlib
 import string
 import random
 import time
+import math
 
 global fresult
 global end
 global str2hash
 global attempt
-fresult = "null"
-str2hash = "null"
-attempt = 0
-end = 1
 
 # answer should be the MD5 hash you make- this one is "test"
-answer = "098f6bcd4621d373cade4e832627b4f6"
+answer = "1158f5f8d240a731d28068742adea0fd"
+hash = input("MD5 Hash: ")
+hashlength = int(input("Hash length: "))
+
+
+def premutation_count():
+    count = 230843697339241380472092742683027581083278564571807941132288000000000000 / math.factorial(54 - hashlength)
+
+    print("POSSIBLE PREMUTATIONS: " + str(count))
+    print("SOLVING...")
+
+
+premutation_count()
 
 start = time.time()
 
@@ -23,21 +32,20 @@ def random_char(y):
 
 
 while fresult != answer:
-    str2hash = random_char(4)
+    str2hash = random_char(hashlength)
     result = hashlib.md5(str2hash.encode())
     fresult = result.hexdigest()
     attempt = attempt + 1
-    end = time.time()
-    print("ATTEMPT:", attempt, "NOT SOLVED:", fresult, str2hash)
 
 while fresult == answer:
     end = time.time()
     elapsed = round(end - start, 2)
     hashrate2 = round(attempt / elapsed, 2)
-    print("HASH SOLVED")
-    print("DECODED HASH:", str2hash)
-    print("SECONDS ELAPSED:", str(elapsed))
-    print("HASHES PER SECOND", hashrate2)
-    exit()
+    print("HASH SOLVED...")
+    print("DECODED HASH: " + str2hash)
+    print("SECONDS ELAPSED: " + str(elapsed))
+    print("HASHES PER SECOND (APROX): " + "{:,}".format(hashrate2))
+    print("ATTEMPTED HASHES: " + "{:,}".format(attempt))
+    quit()
 
 result = hashlib.md5(str2hash.encode())
